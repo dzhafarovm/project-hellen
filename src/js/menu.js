1cd49595bb5a699bbadbf148eb1d5a3de89967cb
Maksym Dzhafarov <maksim8797@gmail.com>
2021-04-08 02:05:17 +0300

    Bug fixed

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,7 +2,7 @@ var $page = $('html, body');
 $('a[href*="#"]').click(function() {
     $page.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
+    }, 500);
     $('.js-menu-container').removeClass('is-open')
     return false;
 });
